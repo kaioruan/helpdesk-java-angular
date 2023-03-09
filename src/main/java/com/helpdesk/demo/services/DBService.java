082dcfd4eb80ae0884e061b2855c814fb705799a
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-09 16:22:47 -0300

    feat: create endpoint findAll in router tecnico and insert more data in seeders tecnicos

--- src/main/java/com/helpdesk/demo/services/DBService.java
+++ src/main/java/com/helpdesk/demo/services/DBService.java
@@ -27,7 +27,11 @@ public class DBService {
 	public void instanciaDB() {
 		Tecnico tec1 = new Tecnico(null, "Kaio Ruan", "67376263861", "kaio@gmail.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", "123");
+		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", "123");
+		Tecnico tec4 = new Tecnico(null, "Tim Berners-Lee", "162.720.120-39", "lee@mail.com", "123");
+		Tecnico tec5 = new Tecnico(null, "Linus Torvalds", "778.556.170-27", "linus@mail.com", "123");
+		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5));
 		
 		Cliente cli1 = new Cliente(null, "Linus Tovard", "32936826349", "linus@gmail.com", "123");
 		clienteRepository.saveAll(Arrays.asList(cli1));
