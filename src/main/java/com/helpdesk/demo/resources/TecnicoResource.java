8d44261ca61e4ca2f9e231a27343502756c523b5
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-09 14:53:32 -0300

    feat: create router /tecnicos/id to findById method

---
+++ src/main/java/com/helpdesk/demo/resources/TecnicoResource.java
@@ -0,0 +1,25 @@
+package com.helpdesk.demo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helpdesk.demo.domain.Tecnico;
+import com.helpdesk.demo.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+
+	@Autowired
+	private TecnicoService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico obj = this.service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
