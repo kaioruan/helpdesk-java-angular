0d3d63e639a52ec0a0172eda3b25bd9ebf2a7c8c
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-09 14:33:05 -0300

    feat: create profile dev application

---
+++ src/main/java/com/helpdesk/demo/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.helpdesk.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.helpdesk.demo.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+
+	@Bean
+	public boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+}
