a7c385eaca146a62824a75ca4b23e378e4090c3f
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-10 01:14:20 -0300

    feat: create CRUD endpoints clientes

--- src/main/java/com/helpdesk/demo/domain/Cliente.java
+++ src/main/java/com/helpdesk/demo/domain/Cliente.java
@@ -2,17 +2,19 @@ package com.helpdesk.demo.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.helpdesk.demo.domain.enums.Perfil;
+import com.helpdesk.demo.dtos.ClienteDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
 @Entity
 public class Cliente extends Pessoa {
-
 	private static final long serialVersionUID = 1L;
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
@@ -27,6 +29,17 @@ public class Cliente extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
@@ -34,4 +47,5 @@ public class Cliente extends Pessoa {
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
+
 }
---
+++ src/main/java/com/helpdesk/demo/services/ClienteService.java
@@ -0,0 +1,71 @@
+package com.helpdesk.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.helpdesk.demo.domain.Pessoa;
+import com.helpdesk.demo.domain.Cliente;
+import com.helpdesk.demo.dtos.ClienteDTO;
+import com.helpdesk.demo.repositories.PessoaRepository;
+import com.helpdesk.demo.repositories.ClienteRepository;
+import com.helpdesk.demo.services.exceptions.DataIntegrityViolationException;
+import com.helpdesk.demo.services.exceptions.ObjectNotFoundException;
+
+import jakarta.validation.Valid;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validateCpfEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validateCpfEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
+
+	private void validateCpfEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema.");
+		}
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema.");
+		}
+	}
+
+}
