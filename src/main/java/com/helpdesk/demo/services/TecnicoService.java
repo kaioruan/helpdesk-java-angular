debb16ecd0dcef048eb1560b2f5a8b4338a931e6
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-10 00:49:09 -0300

    feat: create endpoint delete /tecnicos

--- src/main/java/com/helpdesk/demo/services/TecnicoService.java
+++ src/main/java/com/helpdesk/demo/services/TecnicoService.java
@@ -40,13 +40,21 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
-	
+
 	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
 		objDTO.setId(id);
 		Tecnico oldObj = findById(id);
 		validateCpfEmail(objDTO);
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
 	}
 
 	private void validateCpfEmail(TecnicoDTO objDTO) {
