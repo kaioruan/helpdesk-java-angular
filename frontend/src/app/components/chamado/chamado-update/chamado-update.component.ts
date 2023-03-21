0b1b50c96baaf6b285be72f1e10b776f8be58964
kaioruan <kaio.ruan@souunit.com.br>
2023-03-21 15:50:01 -0300

    feat: implement HTML AND CSS chamado-update

--- frontend/src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ frontend/src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import swal from 'sweetalert';
+
 
 @Component({
   selector: 'app-chamado-update',
@@ -6,5 +16,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./chamado-update.component.css']
 })
 export class ChamadoUpdateComponent {
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
 
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private chamadoService: ChamadoService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.findAllCLientes();
+    this.findAllTecnicos();
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(() => {
+      swal("Chamado criado com sucesso!");
+      this.router.navigate(['chamados'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          console.log(element.message);
+        });
+      } else {
+        console.log(ex.error.message);
+      }
+    })
+  }
+
+  findAllCLientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid
+     && this.titulo.valid && this.observacoes.valid
+     && this.tecnico.valid && this.cliente.valid
+  }
 }
