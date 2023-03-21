a79feee64cd90edfe03918308f2e48c4768201ee
kaioruan <kaio.ruan@souunit.com.br>
2023-03-21 15:37:40 -0300

    feat: implement logic to create new chamado in database

--- frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Chamado } from 'src/app/models/chamado';
 import { Cliente } from 'src/app/models/cliente';
 import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-chamado-create',
@@ -32,9 +37,43 @@ export class ChamadoCreateComponent {
   tecnico:    FormControl = new FormControl(null, [Validators.required]);
   cliente:    FormControl = new FormControl(null, [Validators.required]);
 
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private chamadoService: ChamadoService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.findAllCLientes();
+    this.findAllTecnicos();
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(() => {
+      swal("Chamado criado com sucesso!");
+      this.router.navigate(['chamados'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          console.log(element.message);
+        });
+      } else {
+        console.log(ex.error.message);
+      }
+    })
+  }
+
+  findAllCLientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
   }
 
   validaCampos(): boolean {
