a116bd31c81f999c1dc32d8cb7ce5377fffda764
kaioruan <kaio.ruan@souunit.com.br>
2023-03-21 15:14:54 -0300

    feat: validate inputs chamado-create

--- frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class ChamadoCreateComponent {
 
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid
+     && this.titulo.valid && this.observacoes.valid
+     && this.tecnico.valid && this.cliente.valid
+  }
 }
