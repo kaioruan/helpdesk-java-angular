f36088635949069449c368bc4de1e887f592c31a
kaioruan <kaio.ruan@souunit.com.br>
2023-05-22 13:56:47 -0300

    test

--- backend-oauth2/src/database/models/index.ts
+++ backend-oauth2/src/database/models/index.ts
@@ -1,4 +1,4 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
 
-export default new Sequelize(config);
+export default new Sequelize('mysql://root:e7VHpwcC13qoUJ7IKLJ1@containers-us-west-197.railway.app:5768/railway', {});
