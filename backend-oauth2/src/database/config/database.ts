4dcc5e9b53c5408ebb1a52488b3d0930f0dc0028
kaioruan <kaio.ruan@souunit.com.br>
2023-05-22 13:07:55 -0300

    test

--- backend-oauth2/src/database/config/database.ts
+++ backend-oauth2/src/database/config/database.ts
@@ -6,7 +6,7 @@ const config: Options = {
   password: process.env.DB_PASS || '123456',
   database: 'oauth2',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3006,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
